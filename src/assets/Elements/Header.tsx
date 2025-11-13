9eddb352fef1f6d1c342f67c038267c93133ae4e
DaneVN <Dane.vN23@gmail.com>
2025-11-13 22:02:55 +0200

    Added new Bee icon + Header nav Added

--- src/assets/Elements/Header.tsx
+++ src/assets/Elements/Header.tsx
@@ -1,9 +1,12 @@
-import beeIcon from "/bee-icon.jpg";
+import { NavLink } from "react-router-dom";
+import beeIcon from "/bee-icon.png";
 function Header() {
   return (
-    <header className="h-1/12 p-4 bg-(--cl-dark-accent) text-(--cl-text-light) flex justify-between items-center">
-      <img src={beeIcon} className="object-contain w-1/4" alt="bee logo" />
-      <h1 className="text-3xl font-bold">Tony NonBEE</h1>
+    <header className="h-3/24 sm:h-[20vw] p-4 bg-(--cl-dark-accent) text-(--cl-text-light) flex justify-between items-center fixed top-0 left-0 z-10">
+      <NavLink to="/" className="w-1/3">
+        <img src={beeIcon} className="object-contain w-1/2" alt="bee logo" />
+      </NavLink>
+      <h1 className="text-l font-bold">Where you can find your needs</h1>
     </header>
   );
 }
