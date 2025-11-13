d3ff3338844df2fd6e1a9f582b1d8000b5ab7552
DaneVN <Dane.vN23@gmail.com>
2025-11-13 22:01:41 +0200

    Cleanup + Home page initial layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
+import Footer from "./assets/Elements/Footer";
 import Header from "./assets/Elements/Header";
 
 function App() {
   return (
     <>
+      <div className="h-screen w-screen bg-(--cl-secondary) relative overflow-hidden">
+        {/* Background image – transparent */}
+        <img
+          src="./background1.jpg"
+          className="absolute inset-0 w-full h-full object-cover opacity-40 pointer-events-none"
+          alt="Background"
+        />
+
+        {/* Overlay content */}
+        <div className="relative z-10 flex flex-col items-center justify-center h-full p-8">
+          <h1 className="text-5xl text-cl-text-light drop-shadow-lg">
+            <strong>BeyondBEE</strong>
+          </h1>
+          <h2 className="text-3xl">Welcomes you</h2>
+        </div>
+      </div>
       <Header />
-      <main className="flex justify-items-center items-center w-fit h-full">
-        <div>
-          <div className="bg-(--cl-primary) p-4">Styled!</div>
-          <div className="bg-(--cl-secondary) p-4">Styled!</div>
-          <div className="bg-(--cl-light-accent) p-4">Styled!</div>
-          <div className="bg-(--cl-dark-accent) p-4">Styled!</div>
-          <div className="bg-(--cl-contrast) p-4">Styled!</div>
-        </div>
-        <div className="text-(--cl-text-dark)">
-          <div className="bg-(--cl-primary) p-4">Styled!</div>
-          <div className="bg-(--cl-secondary) p-4">Styled!</div>
-          <div className="bg-(--cl-light-accent) p-4">Styled!</div>
-          <div className="bg-(--cl-dark-accent) p-4">Styled!</div>
-          <div className="bg-(--cl-contrast) p-4">Styled!</div>
-        </div>
-        <div className="text-(--cl-text-light)">
-          <div className="bg-(--cl-primary) p-4">Styled!</div>
-          <div className="bg-(--cl-secondary) p-4">Styled!</div>
-          <div className="bg-(--cl-light-accent) p-4">Styled!</div>
-          <div className="bg-(--cl-dark-accent) p-4">Styled!</div>
-          <div className="bg-(--cl-contrast) p-4">Styled!</div>
-        </div>
+      <main className="flex justify-items-center items-center w-full h-full">
+        {/* Site navigation options goes here */}
+        {/* Navigation: List of businesses page */}
+        {/* Navigation: About us section */}
+        {/* Navigation: Contact us */}
+        {/* Navigation: Can I add my business to this list? */}
       </main>
+      <Footer />
     </>
   );
 }
