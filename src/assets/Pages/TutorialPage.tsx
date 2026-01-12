7b496d0233737d6c16777504edb3c12e7345a586
DaneVN <Dane.vN23@gmail.com>
2026-01-12 09:16:56 +0200

    Tutorial Page update + Video ID Added

--- src/assets/Pages/TutorialPage.tsx
+++ src/assets/Pages/TutorialPage.tsx
@@ -4,18 +4,22 @@ function TutorialPage() {
       <h1 className="text-6xl text-center w-full text-(--cl-light-accent) mt-32">
         <strong>Tutorials Page</strong>
       </h1>
-      <p className="text-2xl text-(--cl-text-dark) w-3/4 mx-auto mt-16">
+      <p className="text-2xl text-(--cl-text-dark) w-3/4 mx-auto mt-16  text-center">
         Here you can find tutorials on how to use this platform.
       </p>
-      <div id="youtube-videos" className="w-3/4 mx-auto mt-10">
+      <div
+        id="youtube-videos"
+        className="flex flex-col items-center w-3/4 mx-auto mt-10"
+      >
         {/* Example YouTube video embeds */}
-        <div className="mb-8">
+        <div className="mb-8 text-center">
           <h2 className="text-3xl text-(--cl-text-dark) mb-4">
             Listed Businesses Tutorial
           </h2>
-          <div className="aspect-w-16 aspect-h-9">
+          <div className="aspect-w-16 aspect-h-9 flex justify-center">
             <iframe
-              src="https://www.youtube.com/embed/VIDEO_ID"
+              /** https://youtube.com/shorts/B5MFxqzB0p0?feature=share */
+              src="https://www.youtube.com/embed/B5MFxqzB0p0"
               title="Getting Started Tutorial"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
